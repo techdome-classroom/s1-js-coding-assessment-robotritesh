36747b4c1a5f9ea57d4eb27e856c15eb077ddbf2
Ritesh dhote <dhoteritesh66@gmail.com>
2024-09-25 12:36:24 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -5,7 +5,7 @@ const getTotalIsles = function (grid) {
   const cols = grid[0].length;
   let islandCount = 0;
 
-  // Helper function to perform DFS
+  DFS
   const dfs = (i, j) => {
     
     if (i < 0 || i >= rows || j < 0 || j >= cols || grid[i][j] === 'W') return;
